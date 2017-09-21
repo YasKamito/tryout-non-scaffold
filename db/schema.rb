eb4173feb8c8f2d41db99010164c2fc0b45c453a
Yas Kamitokusari <you@example.com>
2017-09-21 22:22:52 +0900

    5-4_complexly_relational_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914133855) do
+ActiveRecord::Schema.define(version: 20170919124303) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -25,6 +31,22 @@ ActiveRecord::Schema.define(version: 20170914133855) do
     t.string "cashtype"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.string "status"
+    t.integer "entry_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "entries", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "blog_id"
   end
 
   create_table "shops", force: :cascade do |t|
